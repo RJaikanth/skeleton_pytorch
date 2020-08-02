7afdbb399dbc861230a2e498cf674fb0d6981c80
Raghhuveer Jaikanth <raghhuveerj97@gmail.com>
2020-08-02 13:44:04 +0530

    add tensorboard graphs, update classification model dict
    Signed-off-by: Raghhuveer Jaikanth <raghhuveerj97@gmail.com>

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,6 +1,5 @@
 from .classification import *
 
-
 CLS_DICT = {
     "alexnet": AlexNet,
     "vgg11": VGG11,
@@ -11,15 +10,15 @@ CLS_DICT = {
     "vgg16_bn": VGG16_BN,
     "vgg19": VGG19,
     "vgg19_bn": VGG19_BN,
-    "resnet18": ResNet18
-    "resnet34": ResNet34
-    "resnet50": ResNet50
-    "resnet101": ResNet101
-    "resnet152": ResNet152
-    "resnext50_32x4d": ResNeXt50_32x4d
-    "resnext50_32x8d": ResNeXt50_32x8d
-    "resnext101_32x8d": ResNeXt101_32x8d
-    "wideresnet50_2": WideResNet50_2
-    "wideresnet101_2": WideResNet101_2
+    "resnet18": ResNet18,
+    "resnet34": ResNet34,
+    "resnet50": ResNet50,
+    "resnet101": ResNet101,
+    "resnet152": ResNet152,
+    "resnext50_32x4d": ResNeXt50_32x4d,
+    "resnext50_32x8d": ResNeXt50_32x8d,
+    "resnext101_32x8d": ResNeXt101_32x8d,
+    "wideresnet50_2": WideResNet50_2,
+    "wideresnet101_2": WideResNet101_2,
     "wideresnet152_2": WideResNet152_2
 }
